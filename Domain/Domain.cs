77d1a223eff3c9150e67d3072d736cf48b304866
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-19 23:17:17 +0530

    domain created

--- Domain/Domain.cs
+++ Domain/Domain.cs
@@ -67,4 +67,176 @@ namespace Guides.Backend.Domain
         public int RoleId { get; set; }
         public Role Role { get; set; }
     }
+
+
+    public class Respondent
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public string FullName { get; set; }
+        public string HusbandName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Telephone1 { get; set; }
+        public string Person1 { get; set; }
+        public string Telephone2 { get; set; }
+        public string Person2 { get; set; }
+        public string Telephone3 { get; set; }
+        public string Person3 { get; set; }
+        public string RCHID { get; set; }
+        public string HospitalId { get; set; }
+        public string PHC { get; set; }
+        public string HealthFacility { get; set; }
+        public string ANM { get; set; }
+        public string ANMTelephone1 { get; set; }
+        public string ANMTelephone2 { get; set; }
+        public string Asha { get; set; }
+        public string AshaTelephone1 { get; set; }
+        public string AshaTelephone2 { get; set; }
+        public MobilePhone OwnAMobilePhone { get; set; }
+        public MobilePhone SecondaryAccessToMobilePhone { get; set; }
+        public ConditionalBoolean SecondaryAccessToSmartphone { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+
+        public SocioDemographic SocioDemographic { get; set; }
+        public int? SocioDemographicId { get; set; }
+
+        public PregnancyAndGdmRiskFactors PregnancyAndGdmRiskFactors { get; set; }
+        public int? PregnancyAndGdmRiskFactorsId { get; set; }
+
+        public TobaccoAndAlcoholUse TobaccoAndAlcoholUse { get; set; }
+        public int? TobaccoAndAlcoholUseId { get; set; }
+
+        public PhysicalActivity PhysicalActivity { get; set; }
+        public int? PhysicalActivityId { get; set; }
+
+        public DietaryBehaviour DietaryBehaviour { get; set; }
+        public int? DietaryBehaviourId { get; set; }
+    }
+
+    public class SocioDemographic
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public Respondent Respondent { get; set; }
+        public int RespondentId { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        
+        //  Domain
+        public DateTime? DateOfBirth { get; set; }
+        public int? Age { get; set; }
+        public Religion Religion { get; set; }
+        public UgandaTribe UgandaTribe { get; set; }
+        public MaritalStatus MaritalStatus { get; set; }
+        public int NPeopleInHousehold { get; set; }
+        public int NRooms { get; set; }
+        public Education RespondentEducation { get; set; }
+        public Education SpouseEducation { get; set; }
+        public Employment Employment { get; set; }
+        public int MonthlyIncome { get; set; }
+        public IncomeSufficiency IncomeSufficiency { get; set; }
+        
+    }
+
+    public class PregnancyAndGdmRiskFactors
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public Respondent Respondent { get; set; }
+        public int RespondentId { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        
+        //  Domain
+
+        public int Gravida { get; set; }
+        public int? Parity { get; set; }
+        public int? Living { get; set; }
+        public GeneralTriplet PreviousBabyWeightOver4kg { get; set; }
+        public GeneralTriplet BabySizeLargerThanAverage { get; set; }
+        public int MonthsFromLastDelivery { get; set; }
+        public DateTime? LMP { get; set; }
+        public DateTime? EDD { get; set; }
+        public int FirstAncVisitMonth { get; set; }
+        public GeneralTriplet WeightMeasuredInCurrentPregnancy { get; set; }
+        public double? Weight { get; set; }
+        public GeneralTriplet HtnOrPreEclampsia { get; set; }
+        public GeneralTriplet Gdm { get; set; }
+        public GeneralTriplet Dm { get; set; }
+        public DiabetesType DiabetesType { get; set; }
+        public GeneralTriplet DmInFamily { get; set; }
+    }
+
+    public class TobaccoAndAlcoholUse
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public Respondent Respondent { get; set; }
+        public int RespondentId { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        
+        //  Domain
+        public bool TobaccoUsed { get; set; }
+        public TobaccoAndAlcoholConsumption Smoking { get; set; }
+        public TobaccoAndAlcoholConsumption OtherTobaccoUse { get; set; }
+        public TobaccoAndAlcoholConsumption Alcohol { get; set; }
+    }
+
+    public class PhysicalActivity
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public Respondent Respondent { get; set; }
+        public int RespondentId { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        
+        //  Domain
+        public int? VigorousActivityDaysPerWeek { get; set; }
+        public int? VigorousActivityHoursPerDay { get; set; }
+        public int? VigorousActivityMinutesPerDay { get; set; }
+        
+        public int? ModerateActivityDaysPerWeek { get; set; }
+        public int? ModerateActivityHoursPerDay { get; set; }
+        public int? ModerateActivityMinutesPerDay { get; set; }
+        
+        public int? WalkingDaysPerWeek { get; set; }
+        public int? WalkingHoursPerDay { get; set; }
+        public int? WalkingMinutesPerDay { get; set; }
+        
+        public int? SittingDaysPerWeek { get; set; }
+        public int? SittingHoursPerDay { get; set; }
+        public int? SittingMinutesPerDay { get; set; }
+        
+    }
+
+    public class DietaryBehaviour
+    {
+        public int Id { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public Respondent Respondent { get; set; }
+        public int RespondentId { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        
+        //  Domain
+        public DietaryBehaviourSelectionA Breakfast { get; set; }
+        public DietaryBehaviourSelectionA RegularMeals { get; set; }
+        public DietaryBehaviourSelectionB Fruits { get; set; }
+        public DietaryBehaviourSelectionB Vegetables { get; set; }
+        public DietaryBehaviourSelectionB Carbohydrates { get; set; }
+        public DietaryBehaviourSelectionB SugaryDrinks { get; set; }
+        public DietaryBehaviourSelectionB Pulses { get; set; }
+        public DietaryBehaviourSelectionB EggsOrDairy { get; set; }
+        public DietaryBehaviourSelectionB FishOrChicken { get; set; }
+        public DietaryBehaviourSelectionB RedMeat { get; set; }
+        public DietaryBehaviourSelectionB Snack { get; set; }
+        public DietaryBehaviourSelectionA OutsideMeals { get; set; }
+    }
 }
--- Domain/Enums.cs
+++ Domain/Enums.cs
@@ -10,4 +10,132 @@ namespace Guides.Backend.Domain
         India,
         Uganda
     }
+
+    public enum GeneralTriplet
+    {
+        Not_applicable,
+        Yes,
+        No,
+        Not_sure
+    }
+
+    public enum ConditionalBoolean
+    {
+        Not_applicable,
+        Yes,
+        No
+    }
+    
+    public enum GeneralQuadruplet
+    {
+        Not_applicable,
+        Yes,
+        No,
+        Do_not_know,
+        No_answer
+    }
+
+    public enum MobilePhone
+    {
+        Not_applicable,
+        No,
+        Yes_a_smartphone,
+        Yes_but_not_a_smartphone
+    }
+    
+    public enum Religion
+    {
+        None,
+        Muslim,
+        Hindu,
+        Christian_non_specific,
+        Seventh_day_adventist_SDA,
+        Catholic,
+        Church_of_Uganda,
+        Pentecostal,
+        Jain,
+        Sikh,
+        Buddhist,
+        Other
+    }
+
+    public enum UgandaTribe
+    {
+        Not_applicable,
+        Muganda,
+        Musoga,
+        Lango,
+        Itesot,
+        Mutooro,
+        Munyankore_Mukiga,
+        Other
+    }
+
+    public enum MaritalStatus
+    {
+        Married_or_cohabiting = 1,
+        Never_married,
+        Separated_divorced,
+        Widow
+    }
+
+    public enum Education
+    {
+        Not_applicable,
+        None_or_early_childhood_education,
+        Primary,
+        Secondary_or_high_school,
+        Graduate_or_post_graduate
+    }
+
+    public enum Employment
+    {
+        Homemaker_or_unemployed = 1,
+        Employed
+    }
+
+    public enum IncomeSufficiency
+    {
+        Yes_it_allows_to_build_savings = 1,
+        Yes_it_allows_to_save_a_little,
+        Yes_it_is_just_enough,
+        No_should_use_savings,
+        No_should_borrow
+    }
+
+    public enum DiabetesType
+    {
+        Not_applicable,
+        Type_1,
+        Type_2,
+        Not_sure
+    }
+
+    public enum TobaccoAndAlcoholConsumption
+    {
+        Not_applicable,
+        Yes_during_this_pregnancy,
+        Yes_but_not_during_this_pregnancy,
+        No
+    }
+
+    public enum DietaryBehaviourSelectionA
+    {
+        Never_or_very_rarely,
+        Less_than_once_a_week,
+        Once_a_week,
+        Weekly_2_to_4_times,
+        Weekly_5_to_6_times,
+        Every_day
+    }
+    
+    public enum DietaryBehaviourSelectionB
+    {
+        Never_or_very_rarely,
+        Once_a_week_or_less_often,
+        Weekly_2_to_4_times,
+        Weekly_5_to_6_times,
+        Daily_1_to_2_times,
+        Daily_3_or_more_times
+    }
 }
